de366a1a500b59d02f6445d58f1c485a64e324d3
ethereumchile <33845595+ethereumchile@users.noreply.github.com>
2018-04-15 23:28:54 -0300

    Update events.js
    Improving syntax

--- Events/events.js
+++ Events/events.js
@@ -1,16 +1,15 @@
-let my_btn = document.getElementById("my_btn");
-my_btn.addEventListener("click",clic,false);
-my_btn.style.float="right";
 
-let my_div = document.getElementById("my_div");
-my_div.addEventListener("click",another_clic,true);
-
-
-function clic(){
+let myButton = document.getElementById("my_btn");
+myButton.style.float = "right";
+myButton.addEventListener("click", () => {
+	
 	alert("This was shown by the button");
-	//my_div.innerHTML = "<p>Hello Dolly.</p>";
-}
 
-function another_clic(){
+}, false);
+
+let myDiv = document.getElementById("my_div");
+myDiv.addEventListener("click", () => {
+
 	alert("This was shown by the div")
-}
+
+}, true);
